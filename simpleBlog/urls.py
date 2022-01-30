dd7ae918e8714fbb917cf16dda3bf4ddfaefebd9
Zubaydullo Abdirakhmonov <z.abdirakhmonov@uic.group>
2022-01-30 15:22:31 +0500

    refactor: some codes regarding api

--- simpleBlog/urls.py
+++ simpleBlog/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('theblog.urls')),
-    # path('members/', include('django.contrib.auth.urls')),
-    # path('members/', include('members.urls')),
+    path('', include('theblog.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
     path("api/v1/", include("api.urls"))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
